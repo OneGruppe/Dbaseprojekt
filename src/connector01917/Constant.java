cc0222e0a7fe67f123e352eea782c4d78de5fe84
Niklaes Jacobsen <niklaes@gmail.com>
2018-04-17 11:57:15 +0200

    new projectUpload

---
+++ src/connector01917/Constant.java
@@ -0,0 +1,16 @@
+package connector01917;
+
+
+// erstat konstanterne nedenfor
+
+public abstract class Constant
+{
+	public static final String
+		server					= "91.100.3.26",  // database-serveren
+		database				=  "projektoplaeg3",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "Eclipse-bruger", // dit brugernavn = dit studienummer 
+		password				= "ySmTL37uDjYZmzyn"; // dit password som du har valgt til din database
+	
+	public static final int
+		port					= 9865;
+}
