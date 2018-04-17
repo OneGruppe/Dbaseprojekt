ae880c7617d1385f4d4e2958ed33c1ebe14686cd
Niklaes Jacobsen <niklaes@gmail.com>
2018-04-17 11:23:09 +0200

    Fix the connection

--- src/connector01917/Constant.java
+++ src/connector01917/Constant.java
@@ -6,11 +6,11 @@ package connector01917;
 public abstract class Constant
 {
 	public static final String
-		server					= "nasdino.myds.me",  // database-serveren
+		server					= "91.100.3.26",  // database-serveren
 		database				=  "projektoplaeg3",  //"jdbcdatabase", // navnet paa din database = dit studienummer
 		username				= "Eclipse-bruger", // dit brugernavn = dit studienummer 
 		password				= "ySmTL37uDjYZmzyn"; // dit password som du har valgt til din database
 	
 	public static final int
-		port					= 59333;
+		port					= 9865;
 }
