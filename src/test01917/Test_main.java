9d385e0eb8947c2e873c25782a443c7f18a9bd92
Sebastian <s170423@student.dtu.dk>
2018-04-24 12:04:02 +0200

    Implementeret interface produktbatch

--- src/test01917/Test_main.java
+++ src/test01917/Test_main.java
@@ -1,8 +1,12 @@
 package test01917;
 
 import daoimpl01917.MySQLOperatoerDAO;
+import daoimpl01917.MySQLProduktBatchDAO;
 import daointerfaces01917.DALException;
 import dto01917.OperatoerDTO;
+import dto01917.ProduktBatchDTO;
+
+import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
 
@@ -15,7 +19,7 @@ public class Test_main {
 		catch (IllegalAccessException e) { e.printStackTrace(); }
 		catch (ClassNotFoundException e) { e.printStackTrace(); }
 		catch (SQLException e) { e.printStackTrace(); }
-		
+		/*
 		System.out.println("Operatoer nummer 3:");
 		MySQLOperatoerDAO opr = new MySQLOperatoerDAO();
 		try { System.out.println(opr.getOperatoer(3)); }
@@ -46,6 +50,16 @@ public class Test_main {
 		System.out.println("Operatoer nummer 5:");
 		try { System.out.println(opr.getOperatoer(5)); }
 		catch (DALException e) { System.out.println(e.getMessage()); }		
+		*/
+		
+		MySQLProduktBatchDAO prod = new MySQLProduktBatchDAO();
+		System.out.println("Produktbatch nummer 1: ");
+		try {
+			new Connector();
+			System.out.println(prod.getProduktBatch(1));
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
 }
