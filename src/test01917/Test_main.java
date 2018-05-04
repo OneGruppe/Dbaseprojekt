f97279208b91f3d3fa5706b152b4cf6d02cb997d
Niklaes Jacobsen <niklaes@gmail.com>
2018-05-04 22:22:01 +0200

    JAVA: Test-main ændret

--- src/test01917/Test_main.java
+++ src/test01917/Test_main.java
@@ -5,25 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import connector01917.Connector;
-import daoimpl01917.MySQLOperatoerDAO;
-import daointerfaces01917.DALException;
-import dto01917.OperatoerDTO;
+import daoimpl01917.*;
+import daointerfaces01917.*;
+import dto01917.*;
 
 public class Test_main {
 	public static void main(String[] args) {
 		try {
 			new Connector();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+			System.out.println(e.getMessage());
 		}
 
-		MySQLOperatoerDAO opr = new	MySQLOperatoerDAO();
+		/**
+		 * 
+		 * OPERATOER 
+		 * 
+		 * CREATE, UPDATE, GETUSER, GETUSERLIST
+		 * 
+		 */
+		OperatoerDAO opr = new	MySQLOperatoerDAO();
 
 		// Create a user (MySQLOperatoer-object, Fornavn, Efternavn, Ini, CPR, Password)
 		createUser(opr, "Yetti", "LastNaim", "YL", "011100-0011", "MyNewPasswordWhichIsSecure");
@@ -33,15 +34,13 @@ public class Test_main {
 
 		// Get a user from CPR (MySQLOperatoer-object, cpr)
 		getUserIdFromCPR(opr, "010195-2012");
-		
+
 		// Get a userList (MySQLOperatoer-object)
 		getUserList(opr);
+
 	}
 	
-	
-	
-
-	public static void createUser(MySQLOperatoerDAO opr, String name, String lname, String ini, String cpr, String pass) {
+	public static void createUser(OperatoerDAO opr, String name, String lname, String ini, String cpr, String pass) {
 		System.out.println("Indsaettelse af ny operatoer");		
 		List<Integer> roles = new ArrayList<Integer>();
 		roles.add(2);
@@ -55,7 +54,7 @@ public class Test_main {
 		}
 	}
 
-	public static void updateUser (MySQLOperatoerDAO opr, String name, String lname, String Ini, String pass) {
+	public static void updateUser (OperatoerDAO opr, String name, String lname, String Ini, String pass) {
 		System.out.println("Opdatering af nuværende operatoer 'Eva'");
 		List<Integer> roles = new ArrayList<Integer>();
 		roles.add(1);
@@ -70,15 +69,15 @@ public class Test_main {
 		}
 	}
 
-	public static void getUserIdFromCPR(MySQLOperatoerDAO opr, String cpr) {
+	public static void getUserIdFromCPR(OperatoerDAO opr, String cpr) {
 		try {
 			System.out.println("Få operatør id fra CPR '010195-2012': " + opr.getOperatoerId("010195-2012"));
 		} catch (DALException e1) {
 			System.out.println(e1.getMessage()); 
 		}
 	}
-	
-	public static void getUserList(MySQLOperatoerDAO opr) {
+
+	public static void getUserList(OperatoerDAO opr) {
 		System.out.println("Alle operatoerer:"); 
 		try {
 			System.out.println(opr.getOperatoerList()); 
@@ -86,4 +85,5 @@ public class Test_main {
 			System.out.println(e.getMessage()); 
 		}
 	}
+	
 }
