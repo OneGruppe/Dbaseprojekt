acb32750f4ec5a926e24b0c012eca6fc9318ee37
Mathias Fager <mathias_1337@hotmail.com>
2018-05-08 12:39:25 +0200

    raavare

--- src/test01917/Test_main_raavare.java
+++ src/test01917/Test_main_raavare.java
@@ -1,5 +1,35 @@
 package test01917;
 
-public class Test_main_raavare {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import connector01917.Connector;
+import daoimpl01917.MySQLRaavareDAO;
+import daointerfaces01917.DALException;
+import daointerfaces01917.RaavareDAO;
+import dto01917.RaavareDTO;
+
+public class Test_main_raavare {
+	static RaavareDAO raav = new MySQLRaavareDAO();
+	
+	public static void createRaavare() {
+		RaavareDTO raavare = new RaavareDTO(0, "Majs", "Majs Levering A/S");
+		try {
+			raav.createRaavare(raavare);
+		} catch (DALException e) {
+			System.out.println(e);
+		}
+	}
+	public static void deleteRaavare() {
+		int id = 0;
+		try {
+			ResultSet rs = Connector.doQuery("SELECT raavare_id FROM raavare WHERE raavare_navn = 'Majs'");
+			while (rs.next()) {
+				id = rs.getInt("raavare_id");
+			}
+			raav.deleteRaavare(id);
+		} catch (DALException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
